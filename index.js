63669f4cbca82b2ba7a9d522945b5ada1263a278
kiyutink <kiyutin@gmail.com>
2020-12-11 19:27:26 +0100

    Delete test command

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const app = require("./app");
 const play = require("./commands/play");
 const joinTeam = require("./actions/join_team");
-const test = require("./commands/test");
 const winner = require("./actions/winner");
 const cancel = require("./actions/cancel");
 const rematch = require("./actions/rematch.");
@@ -12,6 +11,5 @@ app.action(/winner_.+/, winner);
 app.action(/join_team.+/, joinTeam);
 app.action("cancel", cancel);
 app.action("rematch", rematch);
-app.message("hi", test);
 
 app.start(process.env.PORT || 3000);
