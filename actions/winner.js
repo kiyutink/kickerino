11020a2fc2c69047a40c9cd41f746f679aed66d2
kiyutink <kiyutin@gmail.com>
2020-12-11 23:53:06 +0100

    Keep scores

--- actions/winner.js
+++ actions/winner.js
@@ -12,6 +12,24 @@ module.exports = async ({ say, body, ack, client }) => {
   const activeGameData = activeGame.data();
 
   await api.updateGame(activeGame.id, { winner });
+  const loser = winner === "team1" ? "team2" : "team1";
+  for (const playerId of activeGameData[winner]) {
+    const player = await api.getPlayer(playerId);
+    if (player.exists) {
+      await api.updatePlayer(playerId, { wins: player.data().wins + 1 });
+    } else {
+      await api.updatePlayer(playerId, { wins: 1, losses: 0 });
+    }
+  }
+
+  for (const playerId of activeGameData[loser]) {
+    const player = await api.getPlayer(playerId);
+    if (player.exists) {
+      await api.updatePlayer(playerId, { losses: player.data().losses + 1 });
+    } else {
+      await api.updatePlayer(playerId, { losses: 1, wins: 0 });
+    }
+  }
   await client.chat.update({
     channel: activeGameData.slackChannelId,
     ts: activeGameData.winnerMessageTs,
